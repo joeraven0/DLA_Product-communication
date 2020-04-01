9b844f59403666e3130fdffc83455dd71940e0fd
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-04-01 12:25:20 +0200

    Added connection error exception

--- #2_PY38_tcp2ascii_char_client.py
+++ #2_PY38_tcp2ascii_char_client.py
@@ -7,43 +7,55 @@
 import socket, time
 
 
-outputFormat = True #False=ascii char, True=ascii hex
+outputFormat = False #False=ascii char, True=ascii hex
 NO_READ_STRING = "<STX><CAN><CR><LF>"
 [TCP_IP, TCP_PORT] = ['127.0.0.1',51236]
-goodCount, badCount = (0,)*2
+
 
 asciiTable = [
             '<NUL>','<SOH>','<STX>','<ETX>','<EOT>','<ENQ>','<ACK>','<BEL>',
             '<BS>','<HT>','<LF>','<VT>', '<FF>','<CR>','<SO>','<SI>',
             '<DLE>','<DC1>','<DC2>','<DC3>','<DC4>','<NAK>','<SYN>','<ETB>',
             '<CAN>','<EM>','<SUB>','<ESC>','<FS>','<GS>','<RS>','<US>']   
-   
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-while 1:
-    data=s.recv(1024)
-    if not data:
-        print("No data")        
-        break
-    ascii_char=""
-    ascii_char_buffer = data.decode("utf-8")
-    ascii_hex = " ".join([hex(data[n]) for n in range(len(data))])
-    for n in range (len(data)):
-        if data[n]<=31:
-            ascii_char+=asciiTable[data[n]]
+
+
+try:
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))    
+    print("\nConnected to", TCP_IP, ":",TCP_PORT,"\n")
+ 
+except:
+    print("Not connected")
+
+
+
+
+def sw():
+    goodCount, badCount = (0,)*2
+    goodbadread = ""
+    while 1:
+        data=s.recv(1024)
+        if not data:
+            print("No data")        
+            break
+        ascii_char=""
+        ascii_char_buffer = data.decode("utf-8")
+        ascii_hex = " ".join([hex(data[n]) for n in range(len(data))])
+        for n in range (len(data)):
+            if data[n]<=31:
+                ascii_char+=asciiTable[data[n]]
+            else:
+                ascii_char+=ascii_char_buffer[n]
+        if not outputFormat:
+            print(ascii_char)        
+        if outputFormat:
+            print(ascii_hex)
+        if ascii_char==NO_READ_STRING:
+            badCount +=1
         else:
-            ascii_char+=ascii_char_buffer[n]
-    if not outputFormat:
-        print(ascii_char)        
-    if outputFormat:
-        print(ascii_hex)
-    if ascii_char==NO_READ_STRING:
-        badCount +=1
-    else:
-        goodCount +=1
-        goodbadread = "Good: " + str(goodCount) + " Bad: " + str(badCount)
-    print(goodbadread)
-
+            goodCount +=1
+            goodbadread = "Good: " + str(goodCount) + " Bad: " + str(badCount)
+        print(goodbadread)
+sw()
 s.close()
-
